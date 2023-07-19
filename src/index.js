9c336f9146403d4706464a5db3022c3ea49a7604
hks-hns <business@hks-hns.com>
2023-07-19 20:51:45 +0200

    Added the necessary things for calling the settings function.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const mineflayer = require('mineflayer');
 const { pathfinder, Movements } = require('mineflayer-pathfinder');
 const autoEat = require("mineflayer-auto-eat");
 const fs = require('fs');
+const { setupSettingScreen } = require('./settings.js');
 const {
     handleWhisperEvent,
     handleSpawnEntityEvent,
@@ -15,13 +16,18 @@ const {
     initData
 } = require("./eventHandlers.js");
 
-let dataManager = require('./dataManagment.js');
+// Import the dataManagement module
+let dataManager = require('./dataManagement.js');
+let settings = {};
 
 // Read and split credentials from file
-const credentials = fs.readFileSync("../credentials.txt").toString().split("\n");
 
 let isConnected = false;
 let chatSigning = true;
+
+// Check for the "--settings" command-line argument
+const args = process.argv.slice(2);
+const useSettings = args.includes('--settings');
 
 // Function to connect the bot
 function connect() {
@@ -29,24 +35,30 @@ function connect() {
         return;
     }
 
+    // Check if all required settings are defined
+    if (settings.serverIP === undefined || settings.serverPort === undefined || settings.username === undefined || settings.password === undefined || settings.minecraftVersion === undefined || settings.chatSigning === undefined) {
+        console.log("Please start the bot with the '--settings' argument to set the required settings. Or set them manually in the 'settings.json' file.");
+        process.exit(1);
+    }
+
     // Create bot instance
     const instance = mineflayer.createBot({
-        host: credentials[0],
-        port: 25565,
-        username: credentials[1],
-        password: credentials[2],
-        version: "1.19.2",
+        host: settings.serverIP,
+        port: settings.serverPort,
+        username: settings.username,
+        password: settings.password,
+        version: settings.minecraftVersion,
         auth: 'microsoft',
-        disableChatSigning: !chatSigning
+        disableChatSigning: !settings.chatSigning
     });
 
-    // Load the autoEat plugin
+    // Load the autoEat plugin and pathfinder
     instance.loadPlugin(autoEat.plugin);
     instance.loadPlugin(pathfinder);
 
     // Event handlers
     instance.on('whisper', handleWhisperEvent.bind(null, instance));
-    instance.on('kicked', (reason) => chatSigning = reason.includes("unsigned_chat") ? false : chatSigning);
+    instance.on('kicked', (reason) => settings.chatSigning = reason.includes("unsigned_chat") ? false : settings.chatSigning);
     instance.on('entityGone', handleDespawnEvent.bind(null, instance));
     instance.on('entitySpawn', handleSpawnEntityEvent.bind(null, instance));
     instance.on('spawn', handleSpawnEvent.bind(null, instance).bind(null, isConnected));
@@ -68,6 +80,14 @@ process.on('SIGTERM', handleProgramExit);
 
 // Handle uncaught exceptions
 process.on('uncaughtException', (err) => console.log(err.message));
+
+// If '--settings' argument is provided, implement the settings handling logic here
+if (useSettings) {
+    setupSettingScreen();
+} else {
+    dataManager.loadSettings();
+    settings = dataManager.getSettings();
+}
 
 // Initialize pearlPlayer
 initData();
