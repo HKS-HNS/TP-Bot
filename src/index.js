c65d264636dae2d0bfa0b3eccccc5c2beb535e67
hks-hns <business@hks-hns.com>
2023-07-25 16:25:38 +0200

    Removed unused things

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const mineflayer = require('mineflayer');
 const { pathfinder, Movements } = require('mineflayer-pathfinder');
 const autoEat = require("mineflayer-auto-eat");
 const fs = require('fs');
-const { setupSettingScreen } = require('./settings.js');
 const {
     handleWhisperEvent,
     handleSpawnEntityEvent,
@@ -17,7 +16,7 @@ const {
 } = require("./eventHandlers.js");
 
 // Import the dataManagement module
-let dataManager = require('./dataManagement.js');
+let dataManager = require('./dataManagment.js');
 let settings = {};
 
 // Read and split credentials from file
@@ -37,7 +36,7 @@ function connect() {
 
     // Check if all required settings are defined
     if (settings.serverIP === undefined || settings.serverPort === undefined || settings.username === undefined || settings.password === undefined || settings.minecraftVersion === undefined || settings.chatSigning === undefined) {
-        console.log("Please start the bot with the '--settings' argument to set the required settings. Or set them manually in the 'settings.json' file.");
+        console.log("Please start: node settings.js to set the required settings. Or set them manually in the 'settings.json' file.");
         process.exit(1);
     }
 
@@ -81,13 +80,9 @@ process.on('SIGTERM', handleProgramExit);
 // Handle uncaught exceptions
 process.on('uncaughtException', (err) => console.log(err.message));
 
-// If '--settings' argument is provided, implement the settings handling logic here
-if (useSettings) {
-    setupSettingScreen();
-} else {
-    dataManager.loadSettings();
-    settings = dataManager.getSettings();
-}
+dataManager.loadSettings();
+settings = dataManager.getSettings();
+
 
 // Initialize pearlPlayer
 initData();
